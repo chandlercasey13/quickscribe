892da603a690f1b4f38247e611941d2772f669f4
chandlercasey13 <chandlercasey13@gmail.com>
2025-12-23 14:50:44 -0600

    Init

---
+++ src/main/java/org/chandlercasey/quickscribe/QuickScribeApplication.java
@@ -0,0 +1,13 @@
+package org.chandlercasey.quickscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickScribeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickScribeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chandlercasey/quickscribe/QuickScribeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chandlercasey.quickscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickScribeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
