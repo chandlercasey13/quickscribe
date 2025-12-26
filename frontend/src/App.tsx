2265ba3ae9ab2d104fe5e4ee0d98c09c503a4c8e
chandlercasey13 <chandlercasey13@gmail.com>
2025-12-26 16:25:01 -0600

    Creates simple frontend with login form

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import LoginForm from "./components/login-form";
+
+function App() {
+
+  return (
+    <>
+         <LoginForm className = "w-96" >
+
+        </LoginForm>
+
+
+    </>
+  )
+}
+
+export default App
