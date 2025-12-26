2307030049095a5be7a0ea3905b8c34e3793f028
chandlercasey13 <chandlercasey13@gmail.com>
2025-12-26 06:35:30 -0600

    Creates existsByEmail validation in repository

--- src/main/java/org/chandlercasey/quickscribe/controllers/AuthController.java
+++ src/main/java/org/chandlercasey/quickscribe/controllers/AuthController.java
@@ -1,10 +1,17 @@
 package org.chandlercasey.quickscribe.controllers;
 
+import org.chandlercasey.quickscribe.repositories.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    private final UserRepository userRepository;
+
+    public AuthController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
 
     @GetMapping("/auth/login")
     public String Login(){
@@ -13,6 +20,10 @@ public class AuthController {
 
     @GetMapping("/auth/signup")
     public String SignUp(){
+        if (userRepository.existsByEmail("chandlercasey13@gmail.com")){
+            return "Successful Query";
+        }
+
         return "Signup";
     }
 
